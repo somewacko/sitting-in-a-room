94ed51667127773504eee53a4124d00e8447b2dc
zo7 <zo7.flynn@gmail.com>
2015-08-30 13:23:21 -0400

    Adds argparse to handle command-line arguments
    This also enables setting the level of the convolved signal and whether or not
    it should use 'full' from the command-line as well.

--- sitting-in-a-room.py
+++ sitting-in-a-room.py
@@ -1,25 +1,24 @@
 '''
     sitting-in-a-room.py | zo7
 
-    Runs a process inspired by Alvin Lucier's "I Am Sitting In A Room" for any
-    sound or room.
+    Runs a process inspired by Alvin Lucier's "I Am Sitting In A Room" for a
+    sound in a room defined by its impulse response.
 
     Takes an input signal (x) and an impulse response (h), computing the
-    following system for a non-negative number n:
+    following system for a non-negative integer n:
 
         y(0) = x + a(x*h)
-        y(n) = y(n-1) + a(y(n-1)*h)   (where * is the convolution operator)
+        y(n) = y(n-1) + a(y(n-1)*h)
 
-    Or, it just applies reverb to the signal over and over again
+        (where * is the convolution operator and a is the level of the
+        room's echoes)
 
-    Usage:
-        python sitting-in-a-room.py {input filename} {ir filename} {n}
+    Or more simply, it just applies reverb to the signal over and over again.
 
-        Files must be 1-channel, 16-bit .wav files, ideally with the same
-        sampling rate.
+    Files must be 1-channel, 16-bit .wav files, ideally with the same
+    sampling rate.
 '''
 
-import getopt, os, sys, time
 import numpy as np
 import scipy.io.wavfile as wav
 
@@ -85,17 +84,17 @@ def fft_convolve(a, b):
     b = b.copy()
 
     # Determine the length of the new signal - Take the sum of the lengths of
-    # the signals and go to the next power of 2 (to make FFT run optimally).
+    # the signals and go to the next power of 2 (to make FFT run optimally)
     length = next_pow_2( len(a) + len(b) )
 
-    # Resize and zero-pad the signals for our new length.
+    # Resize and zero-pad the signals for our new length
     a.resize(length)
     b.resize(length)
 
-    # Compute the convolution.
+    # Compute the convolution
     result = np.real(np.fft.ifft( np.fft.fft(a) * np.fft.fft(b) ))
 
-    # Return only the audible portion.
+    # Return only the audible portion
     end = find_end(result)
     return normalize(result[0:end])
 
@@ -103,12 +102,12 @@ def fft_convolve(a, b):
 def stitch_signals(signals):
     ''' Takes a list of numpy arrays and stitches them together. '''
 
-    # TODO: Stitch signals together incrementally, rather than all at once.
+    # TODO: Stitch signals together incrementally, rather than all at once
 
-    # Get the length of the first signal.
+    # Get the length of the first signal
     initial_length = len(signals[0])
 
-    # Initialize an empty array to put the stiched signals in.
+    # Initialize an empty array to put the stitched signals in
     length = len(signals)*initial_length + len(signals[-1])
     stitch = np.zeros(length)
 
@@ -126,6 +125,9 @@ def sitting_in_a_room(input_signal, ir_signal, num_passes,
     ''' Takes an input signal and an impulse response and convolves the signal
         by the impulse convolved 'num_passes' times by itself. '''
 
+    if loud:
+        import sys
+
     signal = input_signal.copy()
 
     if full:
@@ -133,20 +135,20 @@ def sitting_in_a_room(input_signal, ir_signal, num_passes,
 
     for i in range(1, num_passes+1):
 
-        # Display progress message.
+        # Display progress message
         if loud:
             msg = '\rComputing iteration {0:4d}/{1}'.format(i, num_passes)
             sys.stdout.write(msg)
             sys.stdout.flush()
 
-        # Compute the convolution.
+        # Compute the convolution
         convolved = fft_convolve(signal, ir_signal)
 
-        # Mix the last computed signal back in.
+        # Mix the last computed signal back in
         signal.resize(len(convolved))
         signal = signal + conv_level * convolved
 
-        # Normalize the new signal so it doesn't explode.
+        # Normalize the new signal so it doesn't explode
         signal = normalize(signal)
 
         if full:
@@ -167,7 +169,14 @@ def main(input_filename, ir_filename, num_passes, output_filename,
     ''' Main method - Runs 'sitting_in_a_room' for a given input and impulse
         response and writes it to a file. '''
 
-    # Read in files and scale them.
+    import time
+
+    if num_passes < 0:
+        if loud:
+            print('Error: Cannot run negative iterations. Aborting.')
+        return
+
+    # Read in files and scale them
     print('\nReading {0}...'.format(input_filename))
     (r1, input_signal) = wav.read(input_filename)
     print('Reading {0}...'.format(ir_filename))
@@ -176,22 +185,28 @@ def main(input_filename, ir_filename, num_passes, output_filename,
     input_signal = scale_wav(input_signal)
     ir_signal    = scale_wav(ir_signal)
 
-    # Enforce 1-channel signals (for now...).
+    # Enforce 1-channel signals (for now...)
     if len(np.shape(input_signal)) > 1:
-        print('Error: {0} is not mono. Aborting.'.format(input_filename))
+        if loud:
+            print('Error: {0} is not mono. Aborting.'.format(input_filename))
         return
     if len(np.shape(ir_signal)) > 1:
-        print('Error: {0} is not mono. Aborting.'.format(ir_filename))
+        if loud:
+            print('Error: {0} is not mono. Aborting.'.format(ir_filename))
         return
 
     if r1 != r2:
-        print('Warning: sampling rate of {0} and {1} differ.'.format(
-            input_filename, ir_filename))
-        print('         Using sampling rate of {0} ({1})'.format(
-            input_filename, r1))
-    sampling_rate = r1 # Use input signal's sampling rate.
+        if loud:
+            print('Warning: sampling rate of {0} and {1} differ.'.format(
+                input_filename, ir_filename))
+            print('         Using sampling rate of {0} ({1})'.format(
+                input_filename, r1))
+    sampling_rate = r1 # Use input signal's sampling rate
 
-    print('Running "sitting in a room" with {0} passes...'.format(num_passes))
+    if loud:
+        print('Running "sitting in a room" with {0} passes...'.format(num_passes))
+
+    # Run "Sitting In A Room" and time it
 
     runtime = time.time()
 
@@ -206,32 +221,58 @@ def main(input_filename, ir_filename, num_passes, output_filename,
     runtime = time.time()-runtime
 
     write_to_wav(data, sampling_rate, output_filename)
-    print('Result written to {0}'.format(output_filename))
 
-    print('\nFinished in {:0.2f} seconds\n'.format(runtime))
+    if loud:
+        print('Result written to {0}'.format(output_filename))
+        print('\nFinished in {:0.2f} seconds\n'.format(runtime))
 
 
-def print_usage():
-    print(
-        '\n'
-        'Usage:\n'
-        '    python sitting-in-a-room.py {input file} {ir file} {n}\n'
-    )
-
+# ---- Command line invocation
 
 if __name__ == '__main__':
+    import argparse, os
 
-    # Get args, exit if wrong
-    try:
-        input_filename = sys.argv[1]
-        ir_filename    = sys.argv[2]
-        num_passes     = int(sys.argv[3])
-    except:
-        print_usage()
-        sys.exit(2)
+    parser = argparse.ArgumentParser(description='Runs a process inspired by '
+        'Alvin Lucier\'s "I Am Sitting In A Room" for a sound in a room '
+        'defined by its impulse response. Input files must be 1-channel, '
+        '16bit .wav files, ideally with matching sampling rates.')
 
-    output_filename = 'output/out-{0}-{1}-n{2:03d}-full.wav'.format(
-        input_filename[:-4], ir_filename[:-4], num_passes)
+    parser.add_argument(
+        'input_filename',
+        help='Name of the input file, located in "./sound".',
+    )
+    parser.add_argument(
+        'ir_filename',
+        help='Name of the impulse response file, located in "./sound".',
+    )
+    parser.add_argument(
+        '-num_passes',
+        type=int,
+        default=10,
+        help='The number of iterations to process. (Default 10)',
+    )
+    parser.add_argument(
+        '-level',
+        default=0.707,
+        type=float,
+        help='The amount the convolved signal will be scaled. (Default 0.707)',
+    )
+    parser.add_argument(
+        '-full',
+        action='store_true',
+        help='Produces and output of all iterations, rather than just the one '
+             'specified by -num_passes.',
+    )
+
+    args = parser.parse_args()
+
+    # Create an output filename from the input files and number of iterations
+    output_filename = 'output/out-{0}-{1}-n{2:03d}{3}.wav'.format(
+        args.input_filename[:-4],
+        args.ir_filename[:-4],
+        args.num_passes,
+        '-full' if args.full else '',
+    )
 
     # Create output directory if not already there
     if not os.path.exists('output'):
@@ -239,12 +280,12 @@ if __name__ == '__main__':
 
     # Run the program
     main(
-        'sound/'+input_filename,
-        'sound/'+ir_filename,
-        num_passes,
+        'sound/'+args.input_filename,
+        'sound/'+args.ir_filename,
+        args.num_passes,
         output_filename,
-        conv_level = 0.707,
-        full = False,
+        conv_level = args.level,
+        full = args.full,
         loud = True,
     )
 
